ef6e0b13f755efadc1db0a24ac9792e103ff799e
noahcoyle11 <noahcoyle11@vt.edu>
2020-11-13 23:52:23 -0500

    Create Input.java
    Added Input class

---
+++ Project 5/src/prj5/Input.java
@@ -0,0 +1,20 @@
+package prj5;
+/**
+ * 
+ */
+public class Input {
+
+    /**
+     * Create a new Input object.
+     */
+    public Input() {
+        // This method is intentionally empty
+    }
+
+    /**
+     * Main code output
+     */
+    public static void main(String[] str) {
+        //TODO
+    }
+}
