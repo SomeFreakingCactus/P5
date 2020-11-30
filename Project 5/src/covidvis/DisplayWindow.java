491ec9add2b1e23e6fec11dc76d927e0c7a03589
SomeFreakingCactus <49375973+SomeFreakingCactus@users.noreply.github.com>
2020-11-30 13:40:48 -0500

    DisplayWindow skeleton

--- Project 5/src/covidvis/DisplayWindow.java
+++ Project 5/src/covidvis/DisplayWindow.java
@@ -1,17 +1,48 @@
 package covidvis;
+
+import cs2.Button;
+import cs2.Window;
+
 /**
- * DisplayWindow
+ * Visualizes the COVID-19 testing data in a new window.
  * 
- * @author none
- * @version none
+ * @author all
+ * @version 2020.11.30
  */
 public class DisplayWindow {
+    private Window window;
+    private Calculator calc;
+    private Button alpha;
+    private Button cfr;
+    private Button quit;
+    private Button[] stateButtons;
 
     /**
      * Display Window
      */
     public DisplayWindow() {
-        //Nothing done yet
+        // Nothing done yet
     }
 
+    // TODO Figure out a way to implement clickedState without an int parameter.
+
+
+    public void clickedAlpha(Button alpha) {
+        // TODO
+    }
+
+
+    public void clickedCfr(Button cfr) {
+        // TODO
+    }
+
+
+    private void displayState(int index) {
+        // TODO
+    }
+
+
+    private void displayBar(EthnicityGroup group, int pos) {
+        // TODO
+    }
 }
